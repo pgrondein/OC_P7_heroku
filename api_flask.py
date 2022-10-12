7b11f3bc820b4062c8969223c3d581a0f9283fd0
Pascaline Grondein <pascaline.grondein@gmail.com>
2022-10-12 18:20:49 +0200

    add page hello

--- api_flask.py
+++ api_flask.py
@@ -12,9 +12,11 @@ from functions import predict
 
 app = Flask(__name__)
 
-@app.route("/hello", methods=['POST'])
-def hello():
-    print('Hello')
+@app.route("/hello", methods=['GET'])
+def helloworld():
+    if(request.method == 'GET'):
+        data = {"data": "Hello World"}
+        return jsonify(data)
 
 
 @app.route("/predict", methods=['POST'])
